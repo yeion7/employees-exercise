4b07eaf7dc2a361e89169a65189c795fb5bbc498
Yeison Daza <jahir045@gmail.com>
2018-03-03 09:41:48 -0500

    '#add: Row testing'

---
+++ src/components/Row/__mocks__/employ.js
@@ -0,0 +1,11 @@
+const employ = {
+  id: 0,
+  name: 'Eduardo González',
+  company: 'América Móvil',
+  salary: 5400,
+  age: 57,
+  phone: '(886) 271 4211',
+  email: 'eduardo@gmail.com'
+}
+
+export default employ
---
+++ src/components/Row/__test__/Row.test.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+
+import Row from '../'
+import employ from '../__mocks__/employ'
+
+describe('Row component', () => {
+  it('renders correctly with MXN', () => {
+    const fn = jest.fn()
+
+    const tree = renderer
+      .create(
+        <Row
+          id={employ.id}
+          name={employ.name}
+          company={employ.company}
+          salary={employ.salary}
+          age={employ.age}
+          phone={employ.phone}
+          email={employ.email}
+          currency="MXN"
+          changeType={19.9}
+          editable={false}
+          deleteEmploy={fn}
+          onEditField={fn}
+        />
+      )
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders correctly with USD', () => {
+    const fn = jest.fn()
+
+    const tree = renderer
+      .create(
+        <Row
+          id={employ.id}
+          name={employ.name}
+          company={employ.company}
+          salary={employ.salary}
+          age={employ.age}
+          phone={employ.phone}
+          email={employ.email}
+          currency="USD"
+          changeType={19.9}
+          editable={false}
+          deleteEmploy={fn}
+          onEditField={fn}
+        />
+      )
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders correctly with editable mode', () => {
+    const fn = jest.fn()
+    const factory = jest.fn(() => jest.fn())
+
+    const tree = renderer
+      .create(
+        <Row
+          id={employ.id}
+          name={employ.name}
+          company={employ.company}
+          salary={employ.salary}
+          age={employ.age}
+          phone={employ.phone}
+          email={employ.email}
+          currency="USD"
+          changeType={19.9}
+          editable={true}
+          deleteEmploy={fn}
+          onEditField={factory}
+        />
+      )
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
